cdcd18723a2a8b71ed5bf40e37b28fabb9b7d687
AdrianDonate <adrianmc1500@gmail.com>
2021-12-08 10:41:44 +0100

    Commit de prueba

--- Proyecto/src/main/java/B/Proyecto/App.java
+++ Proyecto/src/main/java/B/Proyecto/App.java
@@ -2,6 +2,6 @@ package B.Proyecto;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Primer commit de prueba");
   }
 }
